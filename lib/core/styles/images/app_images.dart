363319360dc0685fc2b1b14addffea539b779bab
bstawy <mohamed.bastawiie@gmail.com>
2024-11-13 21:56:23 +0200

    FECOM-1 Create Splash Screen using lottie animation

--- lib/core/styles/images/app_images.dart
+++ lib/core/styles/images/app_images.dart
@@ -1,4 +1,8 @@
 import 'dart:core';
-class AppImages{
-  static const splash="assets/images/splash.json";
+
+class AppImages {
+  static const _animationsPath = "assets/animations";
+  static const splash = "assets/images/splash.json";
+  static const flowerySplash = '$_animationsPath/flowery_splash_animation.json';
+  static const floweryAnimation = '$_animationsPath/Flowery.json';
 }
