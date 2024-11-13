7618526b81c03847f15f09470a1c590f1e44b066
Khalid Omr <khalidomr805@gmail.com>
2024-11-13 22:11:58 +0200

    FECOM-9-add icons assets .

--- lib/core/styles/images/app_images.dart
+++ lib/core/styles/images/app_images.dart
@@ -1,4 +1,9 @@
 import 'dart:core';
 class AppImages{
-  static const splash="assets/images/splash.json";
+  static const splash = "assets/images/splash.json";
+  static const personIcon = 'assets/images/person_icon.png';
+  static const homeIcon = 'assets/images/home_icon.png';
+  static const categoryIcon = 'assets/images/category_icon.png';
+  static const logo = 'assets/images/Logo.png';
+  static const shoppingIcon = 'assets/images/shopping_icon.png';
 }
