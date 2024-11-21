ff7b564feb102bcbecd0492762cd0f694da2e75f
Khalid Omr <khalidomr805@gmail.com>
2024-11-21 21:08:22 +0200

    FECOM-16 - add pest seller screen , add it to routs and connect api

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../domain/repository/best_seller_repository_impl.dart';
 import '../../presentation/auth/forgot_password/view/widgets/email_verification_widget/email_verification.dart';
 import '../../presentation/auth/forgot_password/view/widgets/forgot_password_widget/forget_password_screen.dart';
 import '../../presentation/auth/forgot_password/view_model/forget_passwoed_cubit.dart';
@@ -8,6 +9,7 @@ import '../../presentation/auth/sign_up/view/sign_up_page.dart';
 import '../../presentation/auth/sign_up/view_model/sign_up_cubit.dart';
 import '../../presentation/auth/view/login/login_screen.dart';
 import '../../presentation/auth/view_model/login/login_cubit.dart';
+import '../../presentation/best_seller/view/best_seller_screen.dart';
 import '../../presentation/home_layout/screens/main_page/main_page.dart';
 import '../../splash/splash_screen.dart';
 import '../di/di.dart';
@@ -38,6 +40,14 @@ class AppRoutes {
           builder: (context) => BlocProvider(
             create: (context) => getIt<SignUpCubit>(),
             child: SignUpPage(),
+          ),
+        );
+
+      case PageRouteName.bestSeller:
+        return MaterialPageRoute(
+          builder: (context) => BlocProvider(
+            create: (context) => getIt<BestSellerViewModel>(),
+            child:  BestSellerScreen(),
           ),
         );
 
