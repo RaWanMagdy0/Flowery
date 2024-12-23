f84e52ffa672a2a3e073ff58035fa62076ddf457
RaWan Magdy <rawan.magdy.fahmy@gmail.com>
2024-12-24 01:48:40 +0200

    FECOM-34 update filter

--- lib/presentation/home_layout/screens/categories/view/widgets/custom_search_bar.dart
+++ lib/presentation/home_layout/screens/categories/view/widgets/custom_search_bar.dart
@@ -1,6 +1,6 @@
+import 'package:flowery/presentation/home_layout/screens/categories/view/widgets/filter_sort_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../../../../core/styles/colors/app_colors.dart';
 import '../../../../widgets/search_bar_widget.dart';
 
@@ -35,7 +35,18 @@ class CustomSearchBar extends StatelessWidget {
               size: 25,
             ),
             onPressed: () {
-              // Add menu button functionality here
+              showModalBottomSheet(
+                context: context,
+                backgroundColor: Colors.transparent,
+                isScrollControlled: true,
+                builder: (context) => FilterSortSheet(
+                  currentSort: null,
+                  onSortSelected: (sortOption) {
+                    print("Selected Sort Option: $sortOption");
+
+                  },
+                ),
+              );
             },
             padding: const EdgeInsets.all(12),
           ),
